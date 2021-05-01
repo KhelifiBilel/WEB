062cdebccfbb0570a2456cb4b3dbdcd9f7f03e03
eya <eyaabid90@gmail.com>
2021-05-01 11:16:48 +0100

    laravel connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignComponent } from './sign/sign.component';
-
+//import { DirectiveDirective } from './directive.directive';
+//import { AccueilComponent } from './accueil/accueil.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
     SignComponent,
+   // DirectiveDirective,
+   // AccueilComponent,
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,41 +1,85 @@
 import { HtmlAstPath } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
   styleUrls: ['./sign.component.css']
 })
+  
 export class SignComponent implements OnInit {
-   offset="";
+  //uses here
+  offset = "";
+  form: FormGroup;
+  submitted = false;
+  data: any;
+  dynamique = "form";
+  Confirmer = "Confirmer";
+  bien = "";
+  forgot_pass = false;
+  ok = false;
+  username = "";
 
-   movee(){  this.animation="container sign-up-mode"; 
-            }
-   move(){  this.animation="container";  }
-  animation="container";
-  
-  dynamique="form";
-  Confirmer="Confirmer";
-  bien="";
-  forgot_pass=false;
-  forgot() {this.forgot_pass=true;}
-  signin(){this.forgot_pass=false;}
-  modifierClasse(){    this.dynamique="form loading";
-  this.Confirmer="";
-  this.bien="Récupération en cours";
+  //functions here
 
-   let x= setTimeout(()=>{
-      this.dynamique="form loading ok";
-      this.bien="Bienvenue ";
-    },4000);}
-  
-  constructor() {     let y=setTimeout(()=>{this.ok=true},2000);
-}
-   ok=false;
-  username="";
-  recupererid(input){console.log(input.value);
-    this.username=input.value;}
-  ngOnInit(): void {
+  movee() {
+    this.animation = "container sign-up-mode";
   }
 
+  move() { this.animation = "container"; }
+  animation = "container";
+  
+  
+  forgot() { this.forgot_pass = true; }
+
+  signin() { this.forgot_pass = false; }
+
+  modifierClasse() {
+    this.dynamique = "form loading";
+    this.Confirmer = "";
+    this.bien = "Récupération en cours";
+    let x = setTimeout(() => {
+      this.dynamique = "form loading ok";
+      this.bien = "Bienvenue ";
+    }, 4000);
+  }
+  
+  constructor(private formBuilder: FormBuilder, private dataservice: DataService,
+    private toastr: ToastrService) {} 
+
+ 
+  recupererid(input) {
+    console.log(input.value);
+    this.username = input.value;
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: [''],
+    })
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.dataservice.registerUser(this.form.value).subscribe(res => {
+      this.data = res;
+      console.log(res);
+    });
+
+  }
 }
